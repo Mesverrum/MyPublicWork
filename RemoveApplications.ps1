d3e7b425e1fa66d0067e663f572638c0cf820f93
Marc Netterfield <marcnetterfield@yahoo.com>
2020-06-09 15:01:13 -0700

    Create RemoveApplications.ps1

---
+++ RemoveApplications.ps1
@@ -0,0 +1,36 @@
+
+<#------------- CONNECT TO SWIS -------------#>
+# load the snappin if it's not already loaded (step 1)
+if (!(Get-PSSnapin | Where-Object { $_.Name -eq "SwisSnapin" })) {
+    Add-PSSnapin "SwisSnapin"
+}
+
+#define target host and credentials
+
+$hostname = 'p054slwapps01.epiqcorp.com'
+$user = "amer\ward_marc.netterfiel"
+$password = "MrcNttrfld123!@#"
+# create a connection to the SolarWinds API
+$swis = connect-swis -host $hostname -username $user -password $password -ignoresslerrors
+#$swis = Connect-Swis -Hostname $hostname -Trusted
+
+<#------------- ACTUAL SCRIPT -------------#>
+
+# build the query in SWQL
+$query = @"
+select a.name, a.ApplicationID
+from orion.apm.Application a
+where a.node.caption like '%slwapps01%' and a.name = 'Microsoft IIS'
+"@
+
+# run the query and assign the results to the $applications array
+$applications = Get-SwisData $swis $query
+
+# iterate over the array
+foreach ($app in $applications) {
+    # write out which application we're working with
+    "Working with application: $($app.name)..."
+
+    # delete the application
+    Invoke-SwisVerb $swis 'Orion.APM.Application' 'DeleteApplication' $app.applicationid
+    }
